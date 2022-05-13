fb6a74af6a97675b1e40d65e0f7d0d44b2370019
Emil Eriksson <emil.eriksson@codemill.se>
2022-05-13 13:36:35 +0200

    Add backlinks to entry-show view

---
+++ lisp_parser.py
@@ -0,0 +1,102 @@
+import csv
+import re
+import sys
+import unittest
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Union
+from typing import Optional
+
+def _parse_value(value: str) -> Any:
+    if value == "nil":
+
+        return None
+
+    try:
+        n = int(value)
+
+        return n
+
+    except ValueError:
+        pass
+
+    list_data = next(csv.reader([value[1:-2]], delimiter=' ', quotechar='"'))
+
+    return list_data
+
+
+def parse(input: str) -> Any:
+    result = re.match('^\(:outline (?P<outline>.*) :point (?P<point>nil|\d+)\)$', input)
+    #print(result)
+    if result:
+        return {
+            'outline': _parse_value(result['outline']),
+            'point': _parse_value(result['point']),
+        }
+
+
+def main() -> None:
+    print(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
+
+
+class TestParse(unittest.TestCase):
+    def test_all_nil_should_return_nones(self):
+        # Given
+        input: str = '(:outline nil :point nil)'
+
+        # When
+        result: Any = parse(input)
+
+        # Then
+        expected = {
+            'outline': None,
+            'point': None,
+        }
+        self.assertEqual(expected, result)
+
+    def test_given_point_should_return_none_and_number(self):
+        # Given
+        input: str = '(:outline nil :point 42)'
+
+        # When
+        result: Any = parse(input)
+
+        # Then
+        expected = {
+            'outline': None,
+            'point': 42,
+        }
+        self.assertEqual(expected, result)
+
+    def test_given_outline_should_return_list_and_none(self):
+        # Given
+        input: str = '(:outline ("foo") :point nil)'
+
+        # When
+        result: Any = parse(input)
+
+        # Then
+        expected = {
+            'outline': ["foo"],
+            'point': None,
+        }
+        self.assertEqual(expected, result)
+
+    def test_given_normal_data_should_return_list_and_number(self):
+        # Given
+        input: str = '(:outline ("foo" "bar" "plopp ploop") :point 312)'
+
+        # When
+        result: Any = parse(input)
+
+        # Then
+        expected = {
+            'outline': ["foo", "bar", "plopp ploop"],
+            'point': 312,
+        }
+        self.assertEqual(expected, result)
--- main.py
+++ main.py
@@ -2,15 +2,21 @@
 
 import asyncio
 import os.path
+from dataclasses import dataclass
+from typing import List
 from typing import Optional
+from typing import Sequence
 
 import aiosqlite
 import tornado.ioloop
 import tornado.web
 
+from lisp_parser import parse as lisp_parser
+
+ORG_DB_PATH = os.path.expanduser("~/.emacs.d/.local/etc/org-roam.workaround.db")
 ENTRY_DIRECTORY = os.path.expanduser("~/Brain/")
 # https://github.com/ttscoff/MarkedCustomStyles/blob/master/Bear.css
-STYLE_PATH = os.path.join(ENTRY_DIRECTORY, "Bear.css")
+STYLE_PATH = os.path.join('static', "Bear.css")
 
 # TODO: Async DB-connection
 # https://pypi.org/project/aiosqlite/
@@ -18,7 +24,7 @@ STYLE_PATH = os.path.join(ENTRY_DIRECTORY, "Bear.css")
 # TODO: argparse
 
 
-async def convert_org_to_html(path: str):  # {{{
+async def convert_org_to_html(path: str, input: Optional[bytes] = None):  # {{{
     """Async convert org to html and return string.
     
     https://docs.python.org/3/library/asyncio-subprocess.html
@@ -27,19 +33,22 @@ async def convert_org_to_html(path: str):  # {{{
         "--standalone",
         "--shift-heading-level-by=1",
         "--wrap=preserve",
+        "--no-highlight",
+        "--include-before-body=templates/org-nav.html",
         "--css=/style.css",
         "--from=org",
         "--to=html",
         path,
-    ]
+    ] + (["--include-after-body=/dev/stdin"] if input else [])
     process = await asyncio.create_subprocess_exec(
         "pandoc",
         *cmd,
+        stdin=asyncio.subprocess.PIPE,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
     )
 
-    stdout, stderr = await process.communicate()
+    stdout, stderr = await process.communicate(input=input)
 
     if stderr:
         print(stderr)
@@ -62,30 +71,82 @@ async def db_connection() -> aiosqlite.core.Connection:
 
 
 # Models {{{
+# class Backlink {{{
+@dataclass
+class Backlink:
+    source: str
+    title: str
+    outline: List[str]
+    point: Optional[int]
 
 #}}}
+# class Entry {{{
+@dataclass
+class Entry:
+    title: str
+    file: str
 
+    @property
+    def path(self) -> str:
+        return os.path.join(ENTRY_DIRECTORY, self.file)
 
+    async def backlinks(self, ) -> Sequence:
+        db = await db_connection()
+        sql_string = """SELECT
+            REPLACE(TRIM(source, '"'), ?, '') AS source,
+            TRIM(titles.title, '"') AS title,
+            properties
+        FROM links
+            LEFT JOIN titles ON links.source = titles.file
+        WHERE type = '"file"' AND dest = ?"""
+        query = db.execute(sql_string, (
+            ENTRY_DIRECTORY,
+            f'"{self.path}"',
+        ),)
+        async with query as cursor:
+            links = await cursor.fetchall()
 
-# Controllers {{{
-class OrgHandler(tornado.web.RequestHandler):  # {{{
-    async def get(self, entry: str) -> None:
+        return [Backlink(source=link['source'], title=link['title'],
+                         **lisp_parser(link['properties']))
+                for link in links]
 
-        entry_path = os.path.join(ENTRY_DIRECTORY, entry)
-        stdout = await convert_org_to_html(entry_path)
-
-        self.write(stdout)
-# }}}
-class OrgListHandler(tornado.web.RequestHandler):  # {{{
-    async def get(self) -> None:
+    @classmethod
+    async def list(cls):
         db = await db_connection()
         sql_string = """SELECT
             TRIM(title, '"') AS title,
             REPLACE(TRIM(file, '"'),?,'') AS file
         FROM titles;"""
-        async with db.execute(sql_string, (ENTRY_DIRECTORY,)) as cursor:
+        query = db.execute(sql_string, (ENTRY_DIRECTORY,))
+        async with query as cursor:
             entries = await cursor.fetchall()
-            self.render("org-list.html.j2", entries=entries)
+
+        return [cls(**entry) for entry in entries]
+#}}} #}}}
+
+
+# Controllers {{{
+class OrgHandler(tornado.web.RequestHandler):  # {{{
+    async def get(self, entry: str) -> None:
+        # FIXME: Prevent directory traversal
+        # TODO: Make backlinks into tree-structure
+        # (Same file should only have one top level entry)
+
+        instance = Entry(title="", file=entry)
+
+        endmatter = None
+        backlinks = await instance.backlinks()
+        if backlinks:
+            endmatter = self.render_string("org-backlinks.html", backlinks=backlinks)
+        stdout = await convert_org_to_html(instance.path, endmatter)
+
+        self.write(stdout)
+# }}}
+class OrgListHandler(tornado.web.RequestHandler):  # {{{
+    async def get(self) -> None:
+        # TODO: Add tags to list
+        entries = await Entry.list()
+        self.render("org-list.html", entries=entries)
 #}}} #}}}
 
 def make_app() -> tornado.web.Application:
@@ -95,8 +156,8 @@ def make_app() -> tornado.web.Application:
         [
             url(r"/style.css()", tornado.web.StaticFileHandler,
                 {"path": STYLE_PATH}),
-            url(r"/entries/", handler=OrgListHandler, name="entries-list"),
-            url(r"/entries/(?P<entry>.+)", handler=OrgHandler, name="entries-show"),
+            url(r"/entries/", handler=OrgListHandler, name="entry-list"),
+            url(r"/entries/(?P<entry>.+)", handler=OrgHandler, name="entry-show"),
         ],
         template_path="templates",
     )
